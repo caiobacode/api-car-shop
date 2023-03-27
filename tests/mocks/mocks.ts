bccaa02f6fd5023783c86d2831a27c03c5e547dd
caiobacode <caioodsf@gmail.com>
2023-03-27 13:35:38 -0300

    feat: 48% test coverage, camada services - Req 3

---
+++ tests/mocks/mocks.ts
@@ -0,0 +1,45 @@
+const carReqMock = {
+  model: 'Marea',
+  year: 2002,
+  color: 'Black',
+  status: true,
+  buyValue: 15.990,
+  doorsQty: 4,
+  seatsQty: 5,
+};
+
+const carResMock = {
+  id: '6348513f34c397abcad040b2',
+  model: 'Marea',
+  year: 2002,
+  color: 'Black',
+  status: true,
+  buyValue: 15.990,
+  doorsQty: 4,
+  seatsQty: 5,
+};
+
+const carsListMock = [
+  {
+    id: '6348513f34c397abcad040b2',
+    model: 'Marea',
+    year: 2002,
+    color: 'Black',
+    status: true,
+    buyValue: 15.990,
+    doorsQty: 4,
+    seatsQty: 5,
+  },
+  {
+    id: '6348513f34c397abcad040b2',
+    model: 'Tempra',
+    year: 1995,
+    color: 'Black',
+    status: true,
+    buyValue: 39.000,
+    doorsQty: 2,
+    seatsQty: 5,
+  },
+];
+
+export { carReqMock, carResMock, carsListMock };
---
+++ tests/unit/Services/CarsService.test.ts
@@ -0,0 +1,40 @@
+import Sinon from 'sinon';
+import { expect } from 'chai';
+
+import { Model } from 'mongoose';
+
+import CarsService from '../../../src/Services/CarsService';
+import { carReqMock, carResMock, carsListMock } from '../../mocks/mocks';
+
+describe('CarsService tests', function () {
+  afterEach(function () {
+    Sinon.restore();
+  });
+  it('endpoint POST "/cars"', async function () {
+    Sinon.stub(Model, 'create').resolves(carResMock);
+
+    const carsService = new CarsService();
+    
+    const response = await carsService.createCar({ ...carReqMock });
+    
+    expect(response).to.be.deep.equal(carResMock);
+  });
+
+  it('endpoint GET "/cars"', async function () {
+    Sinon.stub(Model, 'find').resolves(carsListMock);
+    const carsService = new CarsService();
+    
+    const response = await carsService.getAll();
+    
+    expect(response).to.be.deep.equal(carsListMock);
+  });
+
+  it('endpoint GET "/cars/:id"', async function () {
+    Sinon.stub(Model, 'findById').resolves(carResMock);
+    const carsService = new CarsService();
+    
+    const response = await carsService.getById('6348513f34c397abcad040b2');
+    
+    expect(response).to.be.deep.equal(carResMock);
+  });
+});
